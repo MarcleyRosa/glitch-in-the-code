b893c0d720c131f51ee9f7cb7c2930887c83d201
Lucas Pita Lima <lucaspitalima@gmail.com>
2025-02-05 17:37:00 -0300

    resolução getAllTitles

--- src/getAllTitles.js
+++ src/getAllTitles.js
@@ -1,9 +1,17 @@
 const movies = require("../data/movies");
 
-const getAllTitles = (movies) => {
- 
+const getAllTitles = (movie) => {
+   const arrayTitulos = []
+  for (let i = 0; i < movie.length; i++) {
+    
+    arrayTitulos.push(movie[i].titulo)
+        
+  }
+  
+  return arrayTitulos
 }
 
+getAllTitles(movies)
 module.exports = {
   getAllTitles
 }
