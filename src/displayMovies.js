5485fe014cf2819f25c2a48c629c0e8a70618856
Lucas Pita Lima <lucaspitalima@gmail.com>
2025-02-11 19:05:01 -0300

    resolução displayMovies. (forEach)

--- src/displayMovies.js
+++ src/displayMovies.js
@@ -1,7 +1,12 @@
 const { movies } = require("../data/list1");
 
 const displayMovies = (movies) => {
- 
+  if (movies.length == 1) {
+    return movies.map((m) => { return `Título ${m.titulo} - Diretor: ${m.diretor}` })
+  } 
+  movies.forEach((m) => {
+    console.log(`Título: ${m.titulo} - Diretor: ${m.diretor}`);
+  });
 }
 
 module.exports = {
